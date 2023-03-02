43585cf2ea41d55a4d18ae2bbac0d9283ba8ce59
khrisdain <samuelilesanmi15@gmail.com>
2023-03-02 17:21:18 +0100

    Final commit

--- controller/userControl.js
+++ controller/userControl.js
@@ -6,9 +6,16 @@ export const createUser = asyncHandler(async (req, res) => {
     const findUser = await User.findOne({email: email});
     if(!findUser) {
         //create a new user
-        const newUser =await User.create(req.body) //Object-relational mapping to database /ODM 
+        const newUser = await User.create(req.body) //Object-relational mapping to database /ODM 
         res.json(newUser)
     }else {
         throw new Error("User already exists")
     }
+});
+
+
+export const loginUserControl = asyncHandler( async (req, res) => {
+    //param: email and password
+    const { email, password } = req.body;
+    console.log(email, password)
 });
