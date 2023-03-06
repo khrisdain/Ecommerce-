8ad52425d7aa6445bedaf7c8c72fb9345da88abd
khrisdain <samuelilesanmi15@gmail.com>
2023-03-06 23:22:09 +0100

    Today's commit

--- config/jwtTokens.js
+++ config/jwtTokens.js
@@ -1,5 +1,5 @@
 import jwt from  "jsonwebtoken";
 
 const generateToken = (id) => {
-    return jwt.sign({id}, "Olaisgreayt", )
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "3d"} )
 }
