2cceb1e25dd67a02cfeb29f64111a44908946d10
khrisdain <samuelilesanmi15@gmail.com>
2023-03-08 21:27:29 +0100

    getUser route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser, loginUserControl }  from "../controller/userControl.js";
+import { createUser, loginUserControl, getAllUsers }  from "../controller/userControl.js";
 
 const router = express.Router();
 
 //routes for controllers
 router.post("/register", createUser);
-router.post("/login", loginUserControl)
+router.post("/login", loginUserControl);
+router.get("/all-users", getAllUsers);
 
 export default router; 
