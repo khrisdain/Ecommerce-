5f71b5165b8a1241c1f5d18d751eea1a7573c8ee
khrisdain <samuelilesanmi15@gmail.com>
2023-03-09 23:51:20 +0100

    Updated Route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUserControl, getAllUsers, getUser }  from "../controller/userControl.js";
+import { createUser, loginUserControl, getAllUsers, getUser, deleteUser } from "../controller/userControl.js";
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post("/register", createUser);
 router.post("/login", loginUserControl);
 router.get("/all-users", getAllUsers);
 router.get("/:id", getUser);
-//router.delete("/:id", getUser)
+router.delete("/:id", deleteUser);
 
 export default router; 
