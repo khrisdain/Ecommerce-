a102473b4771689767e4e75bd53c7e91318fdd70
khrisdain <samuelilesanmi15@gmail.com>
2023-03-14 10:00:38 +0100

    fix module bug

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,13 +1,21 @@
-import User from "../models/userModel";
-import { Jwt } from "jsonwebtoken";
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import expressAsyncHandler from "express-async-handler";
+
 
 export const authMiddleware = asyncHandler( async(req, res, next) => {
     let token;
     if(req?.headers?.authorization?.startsWith("Bearer")) {
-        token = req.headers?.authorization?.split(" ")[1];
+        token = req.headers?.authorization?.split(" ")[1]; //second index of bearer token 
+        try{
+            if (token) {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                console.log(decoded)
+            }
+        }catch(error){
+            throw new Error("Not Authorized token expired, pls login again")
+        }
     }else{
-        throw new Error(error)
+        throw new Error("No token is attached to header")
     }
 })
