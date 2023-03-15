69dd5616aa64b24fd1564a40b29daf9b6eea08e4
khrisdain <samuelilesanmi15@gmail.com>
2023-03-15 16:16:02 +0100

    complete authentication middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -10,12 +10,24 @@ export const authMiddleware = asyncHandler( async(req, res, next) => {
         try{
             if (token) {
                 const decoded = jwt.verify(token, process.env.JWT_SECRET)
-                console.log(decoded)
+                const user = await User.findById(decoded?.id);
+                req.user = user; 
+                next();
             }
         }catch(error){
             throw new Error("Not Authorized token expired, pls login again")
         }
     }else{
         throw new Error("No token is attached to header")
+    }
+});
+
+export const isAdmin = asyncHandler( async(req, res) => {
+    const { email } = req.user;
+    const adminUser = await User.findOne(email);
+    if(adminUser.role !== "admin") {
+        throw new Error("Sorry, You are not an Admin!")
+    }else{
+        res.json(adminUser)
     }
 })
