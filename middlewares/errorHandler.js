9669879961034f86afbc634b1d739f6094ab5f40
khrisdain <samuelilesanmi15@gmail.com>
2023-03-19 23:35:53 +0100

    updated error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 //not found 
 export const notFound = (req, res, next) => {
-    const error = new Error(`Not Found: ${req.originalUrl}`); //new error same with error
+    const error = new Error(`Not Found: ${req.originalUrl}`); 
     res.status(404);
     next(error)
 };
