789468d3cf9e92b2383bbef513f8cde11d39ce19
khrisdain <samuelilesanmi15@gmail.com>
2023-03-25 21:31:21 +0100

    todays commit

--- controller/productControl.js
+++ controller/productControl.js
@@ -1,10 +1,25 @@
 import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
 
+
+//Create a new product
 export const createProduct = asyncHandler( async(req, res) => {
     try{
         const newProduct = await Product.create(req.body);
-        res.json(newProduct())
+        res.json(newProduct)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+
+//Get a product 
+export const getAProduct = asyncHandler( async(req, res) => {
+    const {id} = req.params;
+    console.log(id)
+    try{
+        const findProduct = await Product.findById(id);
+        res.json(findProduct)
     }catch(error){
         throw new Error(error)
     }
