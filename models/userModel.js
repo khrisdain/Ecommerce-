75aafb9b156ab2b83d66fb2a1b7940d08dfc8540
khrisdain <samuelilesanmi15@gmail.com>
2023-04-05 23:10:47 +0100

    Todays commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 // Declare the Schema of the Mongo mode
 const userSchema = new mongoose.Schema({
     firstname:{
@@ -42,19 +43,38 @@ const userSchema = new mongoose.Schema({
     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
     refreshToken: {
         type: String,
-    }
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
 }, {
     timestamps: true
 });
 mongoose.set("strictQuery", false)
 
+
+//password state logic
 userSchema.pre('save', async function (next){ //pre middleware executes one after another 
+    if(!this.isModified("password")){
+        next();
+    }
+
     const salt = await bcrypt.genSaltSync(10);
     this.password= await bcrypt.hash(this.password, salt)
 });
 
 userSchema.methods.isPasswordMatched = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password)
+};
+
+userSchema.methods.createPasswordResetToken = async function(){
+    const resettoken = crypto.randomBytes(32).toString('hex') //generates randomByte from crypto module
+    this.passwordResetToken = crypto //store hashed token generated using sha-256 algorithm
+        .createHash('sha256')
+        .update(resettoken)
+        .digest('hex')
+    this.passwordResetExpires = Date.now() * 30 * 60 * 1000; //30miutes to sec by 10^-3 to millisec.
+    return resettoken;
 }
 
 //Export the model
