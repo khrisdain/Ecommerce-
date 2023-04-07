bcb33cef652ed8ab08e6d5e6bc915048e2d041a6
khrisdain <samuelilesanmi15@gmail.com>
2023-04-07 18:13:00 +0100

    Today's commit

---
+++ controller/emailController.js
@@ -0,0 +1,37 @@
+//email gateway that allows making REST reaquest against IMAP and SMPT servers.
+//Nodemailer is a module for Node.js applications to allow easy as cake email sending.
+
+import nodemailer from "nodemailer";
+import asyncHandler from "express-async-handler"
+
+export const sendEmail = asyncHandler( async(data, req, res) => {
+    
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.MAIL_ID, // generated ethereal user(actual user in this case is me)
+          pass: process.env.MP // generated ethereal password
+        },
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+    
+})
