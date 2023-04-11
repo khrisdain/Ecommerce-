7eef318a6d9dbb6aa257dc480b00f57b53f4805f
khrisdain <samuelilesanmi15@gmail.com>
2023-04-11 23:29:45 +0100

    bug fixes at nodemailer, send mail function

--- controller/emailController.js
+++ controller/emailController.js
@@ -8,7 +8,7 @@ export const sendEmail = asyncHandler( async(data, req, res) => {
     
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
+        host: "smtp.gmail.com",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
@@ -19,11 +19,11 @@ export const sendEmail = asyncHandler( async(data, req, res) => {
     
       // send mail with defined transport object
       let info = await transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        from: '"Hey👻" <abc@gmail.com>', // sender address
+        to: data.to, // list of receivers
+        subject: data.subject, // Subject line
+        text: data.text, // plain text body
+        html: data.html, // html body
       });
     
       console.log("Message sent: %s", info.messageId);
