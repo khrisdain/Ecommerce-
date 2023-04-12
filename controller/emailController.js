a85090c568848a97f597ffd102c3714449111656
khrisdain <samuelilesanmi15@gmail.com>
2023-04-12 23:16:13 +0100

    Todays commit

--- controller/emailController.js
+++ controller/emailController.js
@@ -24,12 +24,5 @@ export const sendEmail = asyncHandler( async(data, req, res) => {
         subject: data.subject, // Subject line
         text: data.text, // plain text body
         html: data.html, // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...   
+      });  
 })
