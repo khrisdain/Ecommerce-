5be69b3a79d9ecac9ee07d5ef3f9d576e9edf3a6
khrisdain <samuelilesanmi15@gmail.com>
2023-04-24 00:50:51 +0100

    Update

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,8 +1,13 @@
-import blogs from "../models/blogModel.js";
+import Blog from "../models/blogModel.js";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
 import { validateMongoDBId } from "../utils/validateMongodbId.js";
 
 export const createBlog = asyncHandler( async(req, res) => {
-
+    try{
+        const newBlog = await Blog.create(req.body);
+        res.json(newBlog)
+    }catch(error){
+        throw new Error(error)
+    }
 })
