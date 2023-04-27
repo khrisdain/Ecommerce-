9283b014ca154e502dc2a62cf9acc2b974cc9771
khrisdain <samuelilesanmi15@gmail.com>
2023-04-27 23:41:29 +0100

    Update

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -2,9 +2,10 @@ import express from "express"
 import {
     createBlog
 } from "../controller/blogController.js"
+import { isAdmin} from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
-router.post("/", createBlog)
+router.post("/", isAdmin, createBlog)
 
 export default router;
