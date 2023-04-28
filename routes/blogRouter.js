56ba32e0deb8526fb056a8fd9a4c57bf8dd975b6
khrisdain <samuelilesanmi15@gmail.com>
2023-04-28 16:57:02 +0100

    update

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,13 +1,17 @@
 import express from "express"
 import {
     createBlog,
-    updateBlog
+    updateBlog,
+    getBlog,
+    getAllBlogs
 } from "../controller/blogController.js"
 import { authMiddleware, isAdmin} from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", authMiddleware, isAdmin, createBlog)
-router.put("/update/:id", authMiddleware, isAdmin, updateBlog)
+router.get("/:id", getBlog)
+router.get("/", getAllBlogs)
+router.put("/:id", authMiddleware, isAdmin, updateBlog)
 
 export default router;
