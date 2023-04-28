a1c6797a23575191947a10b734eb98a1d07000f0
khrisdain <samuelilesanmi15@gmail.com>
2023-04-28 09:35:50 +0100

    update

--- controller/blogController.js
+++ controller/blogController.js
@@ -14,3 +14,16 @@ export const createBlog = asyncHandler( async(req, res) => {
         throw new Error(error)
     }
 })
+
+
+export const updateBlog = asyncHandler( async(req, res) => {
+    const { id } = req.params;
+    try{
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {new: true}) //id, update, option parameter(new true updtas the Blog after implementation)
+        res.json(updateBlog);
+    }catch(error){
+        throw new Error(error)
+    }
+});
+
+export 
--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,11 +1,13 @@
 import express from "express"
 import {
-    createBlog
+    createBlog,
+    updateBlog
 } from "../controller/blogController.js"
 import { authMiddleware, isAdmin} from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", authMiddleware, isAdmin, createBlog)
+router.put("/update/:id", authMiddleware, isAdmin, updateBlog)
 
 export default router;
