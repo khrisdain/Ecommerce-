9621483c54fe67b58152011412d9c53924d38fe0
khrisdain <samuelilesanmi15@gmail.com>
2023-05-02 23:36:41 +0100

    Update

--- models/blogModel.js
+++ models/blogModel.js
@@ -22,7 +22,7 @@ var blogSchema = new mongoose.Schema(
           type: Boolean,
           default: false,
         },
-        isDisliked: {
+        isDisLiked: {
           type: Boolean,
           default: false,
         },
