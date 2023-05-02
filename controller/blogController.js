d00ca50c7e8235136c20a5057a8d7b2cbf12acf3
khrisdain <samuelilesanmi15@gmail.com>
2023-05-02 23:36:27 +0100

    Update

--- controller/blogController.js
+++ controller/blogController.js
@@ -116,3 +116,50 @@ export const likeBlog = asyncHandler(async (req, res) => {
     }
   });
 
+
+  export const disLikeBlog = asyncHandler(async (req, res) => {
+    const { blogId } = req.body;
+    validateMongoDBId(blogId);
+    // Find the blog which you want to be liked
+    const blog = await Blog.findById(blogId);
+    // find the login user
+    const loginUserId = req?.user?._id;
+    // find if the user has liked the blog
+    const isDisLiked = blog?.isDisLiked;
+    // find if the user has disliked the blog
+    const alreadyLiked = blog?.likes?.find(
+      (userId) => userId?.toString() === loginUserId?.toString()
+    );
+    if (alreadyLiked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        {
+          $pull: { likes: loginUserId },
+          isLiked: false,
+        },
+        { new: true }
+      );
+      res.json(blog);
+    }
+    if (isDisLiked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        {
+          $pull: { dislikes: loginUserId },
+          isDisliked: false,
+        },
+        { new: true }
+      );
+      res.json(blog);
+    } else {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        {
+          $push: { dislikes: loginUserId },
+          isDisliked: true,
+        },
+        { new: true }
+      );
+      res.json(blog);
+    }
+  });
