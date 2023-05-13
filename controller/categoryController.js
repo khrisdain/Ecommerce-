fb4dc029c59056b9c4e7a8b0d036682643fff72f
khrisdain <samuelilesanmi15@gmail.com>
2023-05-13 19:42:39 +0100

    today's commit

---
+++ controller/categoryController.js
@@ -0,0 +1,26 @@
+import Category from "../models/categoryModel.js";
+import asyncHandler from "express-async-handler";
+import { validateMongoDBId } from "../utils/validateMongodbId.js";
+
+
+//create new product Category: admin only
+export const createCategory = asyncHandler(async(req, res) => {
+    try {
+        const newCategory = await Category.create(req.body);
+        res.json(newCategory)
+    }catch(error){
+        throw new Error(error)
+    }
+});
+
+
+//update p.c: admin
+export const updateCategory = asyncHandler( async(req, res) => {
+    const { id } = req.params;
+    try{
+        const updateCategory = await Category.findByIdAndUpdate(id, req.body,{new: true}); //id, optiton, change on submission
+        res.json(updateCategory)
+    }catch(error){
+        throw new Error(error)
+    }
+})
