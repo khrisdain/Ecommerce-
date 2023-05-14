1cf03a7b0c188c09861d45718c873c44244c21fe
khrisdain <samuelilesanmi15@gmail.com>
2023-05-14 23:28:16 +0100

    update

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     createCategory,
     updateCategory,
+    deleteCategory,
 } from "../controller/categoryController.js"
 import { isAdmin, authMiddleware } from "../middlewares/authMiddleware.js";
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCategory)
 router.put("/:id", authMiddleware, isAdmin, updateCategory)
+router.delete("/:id", authMiddleware, isAdmin, deleteCategory)
 
 export default router;
 
