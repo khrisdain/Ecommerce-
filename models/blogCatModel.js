ef01e7873ec13eb17c7cdff6a5318a495acac3d4
khrisdain <samuelilesanmi15@gmail.com>
2023-05-18 05:50:53 +0100

    todays commit

---
+++ models/blogCatModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+const blogCatModel = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+},{timestamps: true});
+
+//Export the model
+const blogCategory = mongoose.model("BlogCategory", blogCatModel);
+
+export default blogCategory;
