0eb75ee83b9dd98d792333926983754e33bfff3d
khrisdain <samuelilesanmi15@gmail.com>
2023-06-26 21:15:03 +0100

    fixing bug

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = () => {
     try{
-        const connection = mongoose.connect(process.env.MONGODB_URl);
+        const connection = 'mongoose.connect(process.env.MONGODB_URl)';
         console.log("Database connected sucessfully")
     }catch(error){
         console.log("Database error")
