95572b6cab80487b7b6677486e332683a19b0a80
khrisdain <samuelilesanmi15@gmail.com>
2023-08-07 20:14:33 +0100

    Today's PR

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -7,15 +7,19 @@ import {
     deleteBlog,
     likeBlog,
     dislikeBlog,
+    uploadblog
 } from "../controller/blogController.js"
 import { authMiddleware, isAdmin} from "../middlewares/authMiddleware.js"
+import { blogImgResize, uploadPhoto } from "../middlewares/uploadImages.js"
 
 const router = express.Router()
 
 router.post("/", authMiddleware, isAdmin, createBlog)
 router.put("/likes", authMiddleware, likeBlog)
 router.put("/dislikes", authMiddleware, dislikeBlog)
-
+router.put(
+    "/upload"
+)
 router.put("/:id", authMiddleware, isAdmin, updateBlog)
 
 router.get("/:id", getBlog)
