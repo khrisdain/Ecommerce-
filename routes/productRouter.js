ef59b2cb917ecf2f253687bc9e4618acc639b833
khrisdain <samuelilesanmi15@gmail.com>
2023-08-07 20:13:48 +0100

    today's PR

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -7,12 +7,23 @@ import {
     deleteProduct,
     addToWishlist,
     rating,
+    uploadImages,
 } from "../controller/productControl.js";
 import { isAdmin, authMiddleware } from "../middlewares/authMiddleware.js";
+import { productImgResize, uploadPhoto } from "../middlewares/uploadImages.js";
 
 const router = express.Router()
 
 router.post("/", authMiddleware, isAdmin, createProduct)
+router.put(
+    "/upload/:id", 
+    authMiddleware, 
+    isAdmin, 
+    /*array of images that can be uploaded max of 10 */
+    uploadPhoto.array("images", 10),
+    productImgResize,
+    uploadImages
+);
 router.put("/wishlist", authMiddleware, isAdmin, addToWishlist)
 router.put("/rating", authMiddleware, rating)
 router.put("/:id", authMiddleware, isAdmin, updateProduct)
