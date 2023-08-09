11f41cb58c5b7531a060b77307ec1a9cf903acf8
khrisdain <samuelilesanmi15@gmail.com>
2023-08-09 19:20:27 +0100

    tdays cPR

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -1,13 +1,13 @@
 import multer from "multer";
 import sharp from "sharp";
-import path from "path"
+import * as path from "path"
 
 /*diskStorage is preferred here as it grants total control on storing
 our file on disk( alternatively: MemoryStorage) */
 const multerStorage = multer.diskStorage({
     destination: function ( req, file, cb){
         /*cb hanldes uncommon(overlooked) callback scenarios  */
-        cb(null, path.join(__dirname, "../public/images"));
+        cb(null, path.join( _dirname, "../public/images/"));
     },
     filename: function (req, file, cb){
         const uniqueSuffix = Date.now() + "-"+ Math.round(Math.random() * 1E9);
@@ -19,7 +19,7 @@ const multerFilter = (req, file, cb) => {
     /*startsWith: if sequence of search correlates with object in function when converted 
     to a string.
     mimeType: indicates the nature of a file/documente etc (multipurpose internet mail extention) */
-    if(file.mimeType.startsWith("image")){
+    if(file.mimetype.startsWith("image")){
         cb(null, true)
     }
     else{
