e218049938a0cc643d14827ab16c3b275637e301
khrisdain <samuelilesanmi15@gmail.com>
2023-09-26 17:41:23 +0100

    updated routes for new request

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -13,6 +13,8 @@ import {
     updatePassword,
     forgotPasswordToken,
     resetPassword,
+    loginAdmin,
+    getWishlist,
  } from "../controller/userControl.js";
 import { authMiddleware, isAdmin } from "../middlewares/authMiddleware.js";
 
@@ -22,13 +24,15 @@ const router = express.Router();
 router.post("/register", createUser);
 router.put("/password", authMiddleware, updatePassword)
 router.post("/login", loginUserControl);
+router.post("/admin-login", authMiddleware, isAdmin, loginAdmin)
 router.post("/forgot-password-token", forgotPasswordToken)
 router.put("/reset-password/:token", resetPassword)
 router.get("/all-users", getAllUsers);
 router.get("/refresh", handleRefreshToken);
 router.post("/logout", logout)
 router.get("/:id", authMiddleware, isAdmin, getUser);
-router.delete("/:id", deleteUser);
+router.get("/wishlist", authMiddleware, getWishlist)
+router.delete("/:id", deleteUser)
 router.put("/edit-user", authMiddleware, updatedUser) 
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
