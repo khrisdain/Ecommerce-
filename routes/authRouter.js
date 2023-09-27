8ac8ea520c7746c3a913b4e36c7cc353066e5a76
khrisdain <samuelilesanmi15@gmail.com>
2023-09-27 14:49:09 +0100

    todays commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -15,6 +15,8 @@ import {
     resetPassword,
     loginAdmin,
     getWishlist,
+    saveAddress,
+    useCart,
  } from "../controller/userControl.js";
 import { authMiddleware, isAdmin } from "../middlewares/authMiddleware.js";
 
@@ -24,9 +26,9 @@ const router = express.Router();
 router.post("/register", createUser);
 router.put("/password", authMiddleware, updatePassword)
 router.post("/login", loginUserControl);
+router.post("/cart", useCart);
 router.post("/admin-login", authMiddleware, isAdmin, loginAdmin)
 router.post("/forgot-password-token", forgotPasswordToken)
-router.put("/reset-password/:token", resetPassword)
 router.get("/all-users", getAllUsers);
 router.get("/refresh", handleRefreshToken);
 router.post("/logout", logout)
@@ -34,6 +36,8 @@ router.get("/:id", authMiddleware, isAdmin, getUser);
 router.get("/wishlist", authMiddleware, getWishlist)
 router.delete("/:id", deleteUser)
 router.put("/edit-user", authMiddleware, updatedUser) 
+router.put("/reset-password/:token", resetPassword)
+router.put("/save-address", authMiddleware, saveAddress)
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
